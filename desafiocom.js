d85df381437f314cc8d339adf039014edb2b592f
EliasFlores <elias.leo.flores@gmail.com>
2022-08-15 19:27:27 -0300

    desafio complementario correcto

--- desafiocom.js
+++ desafiocom.js
@@ -4,13 +4,16 @@ let jugadores = parseInt(prompt("Ingrese la cantidad de jugadores"));
 
 
 for (let i = 0; i < jugadores; i++){
+    console.log("Ingrese los datos del jugador nª", i+1);
 
-    let nombres = prompt("Ingrese su nombre");
-    let edad = prompt("Ingrese su edad");
-
+    let nombres = prompt("Ingrese nombre del jugdador");
+    let edad = prompt("Ingrese la edad del jugador");
+    console.log("Registrando jugador.")
+    console.log("Registrando jugador..")
+    console.log("Registrando jugador...")
     if(edad >= 18){
 
-    console.log("Bienvenido/a", nombres);
+    console.log("Bienvenido/a", nombres, "se ha registrado correctamente como el jugador nª: ", i+1);
     }
     else{
         console.log("No pueden ingresar menores de edad");
